109c3ef18aa006fc745ee5fd59839acb48e7ee58
Fabio Gianzanti <pardal@hotmail.com>
2025-09-03 20:18:09 -0300

    :sparkles: step-006: restarting branching

--- README.md
+++ README.md
@@ -13,3 +13,7 @@
 - step_004: reward to stay "frozen" (penalty to control cost and reduced margin of z, from 0.265-0.310 to 0.275-0.300)
 
 - step_005: reward to walk forward
+
+## Restarting branching
+
+- step_006: first running on training environment, without rewards
--- src/robofei/env/darwin_op3.py
+++ src/robofei/env/darwin_op3.py
@@ -35,11 +35,11 @@ class DarwinOp3Env(MujocoEnv, EzPickle):
         frame_skip: int = 5,
         default_camera_config: Dict[str, Union[float, int]] = DEFAULT_CAMERA_CONFIG,
         healthy_z_range: Tuple[float, float] = (0.275, 0.300),
-        keep_alive_reward: float = 1.0,  # 0.1
-        ctrl_cost_weight: float = 1e-3,  # 5e-2,
-        target_distance: float = 2.0,  # 5.0
-        forward_velocity_weight: float = 1.0,  # 2.50,
-        reach_target_reward: float = 100.0,  # 10000.0,
+        keep_alive_reward: float = 0.0 #1.0,  # 0.1
+        ctrl_cost_weight: float = 0.0 # 1e-3,  # 5e-2,
+        target_distance: float = 0.0 #2.0,  # 5.0
+        forward_velocity_weight: float = 0.0 #1.0,  # 2.50,
+        reach_target_reward: float = 0.0 #100.0,  # 10000.0,
         # pos_deviation_weight: float = 10.0,  # 5e-2,
         # lateral_velocity_weight: float = 5.0,  # 5e-2,
         motor_max_torque: float = 3.0,  # 3.0,
@@ -170,11 +170,11 @@ class DarwinOp3Env(MujocoEnv, EzPickle):
         return reward, reward_info
 
     def termination(self):
-        if not self.is_healthy:
-            return True
+        # if not self.is_healthy:
+        #     return True
 
-        if self.data.qpos[0] >= self._target_distance:
-            return True
+        # if self.data.qpos[0] >= self._target_distance:
+        #     return True
 
         return False
 
